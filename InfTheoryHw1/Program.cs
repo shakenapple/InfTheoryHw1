36f3733cf369c12c02752c9247f4f672ede16237
Polina <xweird.sun@gmail.com>
2016-10-17 17:50:03 +0300

    seq

--- InfTheoryHw1/Program.cs
+++ InfTheoryHw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,44 @@ namespace InfTheoryHw1
     {
         static void Main(string[] args)
         {
+            using (var analizer = new StatisticsAnalyzer(@"C:\PROGL"))
+            {
+                var a = analizer.GetSeqFrequences(1);
+            }
+        }
+    }
+
+    class StatisticsAnalyzer : IDisposable
+    {
+        private FileStream _reader;
+
+        public StatisticsAnalyzer(string path)
+        {
+            _reader = File.OpenRead(path);
+        }
+
+        public Dictionary<string, int> GetSeqFrequences(int seqLength)
+        {
+            byte[] symbs = new byte[seqLength];
+            var lastPos = 0;
+            var result = new Dictionary<string, int>();
+            while (_reader.CanRead)
+            {
+                _reader.Read(symbs, lastPos, seqLength);
+                var str = Encoding.Default.GetString(symbs);
+                if (!result.ContainsKey(str))
+                {
+                    result.Add(str,0);
+                }
+                result[symbs.ToString()] ++;
+                lastPos += seqLength;
+            }
+            return result;
+        }
+
+        public void Dispose()
+        {
+            _reader.Dispose();
         }
     }
 }
